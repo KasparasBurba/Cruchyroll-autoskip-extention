fa9b76ad851cd0187301447b9364f2c7d9b70f18
KasparasBurba <kasparasburba1@gmail.com>
2026-06-16 15:22:39 +0300

    Refactor skip logic, cleaning

--- content.js
+++ content.js
@@ -1,40 +1,41 @@
-console.log("Extension loaded");
-const interval = setInterval(() =>{
-const button = document.querySelector('[aria-label="Skip Recap"], [aria-label="Skip Intro"], [aria-label="Skip Credits"]');
-if (button){
-if (button.getAttribute("aria-label") === "Skip Recap"){
-    chrome.storage.sync.get(["skipRecap"], (result) =>{
-        if(result.skipRecap === true){
-            if (button.getAttribute("aria-hidden") === "false"){
-                const time = setTimeout(() =>{
-                    button.click();
-                },150);
-            }
-        
-        }
-    });
+console.log("Extension loaded")
+
+function skip(button) {
+    if (button.getAttribute("aria-hidden") === "false") {
+        setTimeout(() => {
+            button.click()
+        }, 2)
+    }
 }
-if (button.getAttribute("aria-label") === "Skip Intro"){
-    chrome.storage.sync.get(["skipIntro"], (result) =>{
-        if(result.skipIntro === true){
-            if (button.getAttribute("aria-hidden") === "false"){
-                const time = setTimeout(() =>{
-                    button.click();
-                },150);
-            }
+
+const interval = setInterval(() => {
+    const button = document.querySelector('[aria-label="Skip Recap"], [aria-label="Skip Intro"], [aria-label="Skip Credits"]');
+
+    if (button) {
+        const label = button.getAttribute("aria-label")
+
+        if (label === "Skip Recap") {
+            chrome.storage.sync.get(["skipRecap"], (result) => {
+                if (result.skipRecap) {
+                    skip(button)
+                }
+            })
         }
-    });
-}
-if (button.getAttribute("aria-label") === "Skip Credits"){
-    chrome.storage.sync.get(["skipCredits"], (result) =>{
-        if(result.skipCredits === true){
-            if (button.getAttribute("aria-hidden") === "false"){
-                const time = setTimeout(() =>{
-                    button.click();
-                },150);
-            }
+    
+        else if (label === "Skip Intro") {
+            chrome.storage.sync.get(["skipIntro"], (result) => {
+                if (result.skipIntro) {
+                    skip(button)
+                }
+            })
         }
-    });
-};
-}
-},1000);
+    
+        else if (label === "Skip Credits") {
+            chrome.storage.sync.get(["skipCredits"], (result) => {
+                if (result.skipCredits) {
+                    skip(button)
+                }
+            })
+        }
+    }
+}, 1000)
