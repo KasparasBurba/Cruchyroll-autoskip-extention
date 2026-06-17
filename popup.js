fb45897ff2b299af03a0e96471cf978ac7ebcc83
KasparasBurba <kasparasburba1@gmail.com>
2026-06-17 16:02:41 +0300

    ALL checkbox

--- popup.js
+++ popup.js
@@ -1,33 +1,52 @@
-const recap = document.getElementById("recap");
-const intro = document.getElementById("intro");
-const credits = document.getElementById("credits");
+const recap = document.getElementById("recap")
+const intro = document.getElementById("intro")
+const credits = document.getElementById("credits")
+const all = document.getElementById("all")
 
-chrome.storage.sync.get(["skipRecap"], (result) =>{
-    recap.checked = result.skipRecap;
-});
+function updateAllCheckbox () {
+    all.checked = recap.checked && intro.checked && credits.checked
+}
 
-chrome.storage.sync.get(["skipIntro"], (result) =>{
-    intro.checked = result.skipIntro;
-});
+chrome.storage.sync.get(["skipRecap", "skipIntro", "skipCredits"], (result) => {
+    recap.checked = result.skipRecap ?? true
+    intro.checked = result.skipIntro ?? true
+    credits.checked = result.skipCredits ?? true
 
-chrome.storage.sync.get(["skipCredits"], (result) =>{
-    credits.checked = result.skipCredits;
-});
+    updateAllCheckbox()
+})
+
+all.addEventListener("change", () => {
+    recap.checked = all.checked
+    intro.checked = all.checked
+    credits.checked = all.checked
+
+    chrome.storage.sync.set({
+        skipRecap: all.checked,
+        skipIntro: all.checked,
+        skipCredits: all.checked
+    })
+})
 
 recap.addEventListener("change", () => {
+    updateAllCheckbox()
+
     chrome.storage.sync.set({
         skipRecap: recap.checked
-    });
-});
+    })
+})
 
 intro.addEventListener("change", () => {
+    updateAllCheckbox()
+
     chrome.storage.sync.set({
         skipIntro: intro.checked
-    });
-});
+    })
+})
 
 credits.addEventListener("change", () => {
+    updateAllCheckbox()
+
     chrome.storage.sync.set({
         skipCredits: credits.checked
-    });
-});
+    })
+})
